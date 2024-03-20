b6de76c62a9f95cbdae24f801a204cf2025bd093
Sandip-github-1 <potdarsandip50@gmail.com>
2024-03-20 14:52:37 +0530

    Added constant folder and files

---
+++ components/Constants/constant.ts
@@ -0,0 +1,3 @@
+export const LOGIN_URL = 'http://tport.in/tportweb/restport/loginaction';
+export const TPORT_PR_URL = 'http://tport.in/tportweb/restport/getTportPref';
+export const FLEETS_URL = 'http://tport.in/tportweb/restport/getfleets';
